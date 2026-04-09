6d87110b7c79c93a74768808aaceca1f3d8a72a7
Martin Halden <superknighteye@gmail.com>
2026-04-09 12:09:43 +0200

    Setup

---
+++ Source/ValmuenCrashCourse/MainCharacter/MainCharacterController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainCharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALMUENCRASHCOURSE_API AMainCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
